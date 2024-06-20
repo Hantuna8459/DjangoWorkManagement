8e8a8770b832461f8cbf72aba901fa85ccc374a3
Hantuna8459 <55192795+Hantuna8459@users.noreply.github.com>
2024-06-20 18:11:30 +0700

    ad superuser

--- WorkManagement/core/admin.py
+++ WorkManagement/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import (
+    CustomUser,
+    Workspace,
+    Task,
+    )
 
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Workspace)
+admin.site.register(Task)
--- WorkManagement/core/models.py
+++ WorkManagement/core/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     user_id = models.BigAutoField(primary_key=True, null=False)
-    is_staff = None
     phone_number = models.CharField(max_length=10)
     user_image = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     
