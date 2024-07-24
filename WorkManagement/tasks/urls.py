4531901593f996f4a8bdd207bc58354114e80785
Hantuna8459 <55192795+Hantuna8459@users.noreply.github.com>
2024-07-24 10:29:56 +0700

    urls fix

--- WorkManagement/tasks/urls.py
+++ WorkManagement/tasks/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('workspace_list/<uuid:pk>/task_list/', views.task_list, name='task_list'),
     path('task_list/task_create/', views.task_create, name='task_create'),
     path('task_list/<uuid:pk>/task_update/', views.task_update, name='task_update'),
-    path('task_list/<uuid:pk>/task_update/', views.task_delete, name='task_delete'),
+    path('task_list/<uuid:pk>/task_delete/', views.task_delete, name='task_delete'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
